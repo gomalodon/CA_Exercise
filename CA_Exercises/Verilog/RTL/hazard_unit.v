5f5ea90e2014a2288c1e7b7ec8da5c52973f5ae2
Giel Ooghe <r0790882@pc-klas4-8.esat.kuleuven.be>
2023-05-04 10:42:23 +0200

    init

---
+++ CA_Exercises/Verilog/RTL/hazard_unit.v
@@ -0,0 +1,15 @@
+module hazard_unit(
+      input wire [31:0] instruction_ID_EX,
+      input wire [4:0] rd_EX_MEM,
+      input wire mem_read_ID_EX,
+      output reg pc_write,
+      output reg IF_ID_write
+   );
+
+
+always @(*) begin
+   
+end
+   
+endmodule
+
